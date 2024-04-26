71eaedcb8d5ef8617a76e459659f9d72de5f3afd
Mohamed Shebl <hamdysheblm@gmail.com>
2024-04-26 19:28:44 +0300

    + add integration login with backend and set user data in local storage

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,23 +1,59 @@
-import "./login.scss";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import apiRequest from '../../lib/apiRequest';
+import './login.scss';
 
 function Login() {
-  return (
-    <div className="login">
-      <div className="formContainer">
-        <form>
-          <h1>Welcome back</h1>
-          <input name="username" type="text" placeholder="Username" />
-          <input name="password" type="password" placeholder="Password" />
-          <button>Login</button>
-          <Link to="/register">{"Don't"} you have an account?</Link>
-        </form>
-      </div>
-      <div className="imgContainer">
-        <img src="/bg.png" alt="" />
-      </div>
-    </div>
-  );
+	const [error, setError] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+
+	const navigate = useNavigate();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		setIsLoading(true);
+		setError('');
+		const formatDate = new FormData(e.target);
+
+		const email = formatDate.get('email');
+		const password = formatDate.get('password');
+
+		try {
+			const res = await apiRequest.post('/auth/login', {
+				email,
+				password,
+			});
+			localStorage.setItem('user', JSON.stringify(res.data.data.user));
+		} catch (error) {
+			setError(error.response.data.message);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+	return (
+		<div className="login">
+			<div className="formContainer">
+				<form onSubmit={handleSubmit}>
+					<h1>Welcome back</h1>
+					<input name="email" required type="email" placeholder="Email" />
+					<input
+						name="password"
+						required
+						minLength={6}
+						type="password"
+						placeholder="Password"
+					/>
+					<button disabled={isLoading}>Login</button>
+					{error && <span>{error}</span>}
+					<Link to="/register">{"Don't"} you have an account?</Link>
+				</form>
+			</div>
+			<div className="imgContainer">
+				<img src="/bg.png" alt="" />
+			</div>
+		</div>
+	);
 }
 
 export default Login;
