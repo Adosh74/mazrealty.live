70f1ae99ce6f2ce31e5f039c6cc9e32ee3f84f4d
Mohamed Shebl <hamdysheblm@gmail.com>
2024-04-27 16:08:26 +0300

    + add RequireAuth layout for protecting routes

--- client/src/routes/layout/layout.jsx
+++ client/src/routes/layout/layout.jsx
@@ -1,18 +1,41 @@
-import "./layout.scss";
-import Navbar from "../../components/navbar/Navbar"
-import { Outlet } from "react-router-dom";
+import { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import Navbar from '../../components/navbar/Navbar';
+import { AuthContext } from '../../context/authContext';
+import './layout.scss';
 
 function Layout() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <Outlet/>
-      </div>
-    </div>
-  );
+	return (
+		<div className="layout">
+			<div className="navbar">
+				<Navbar />
+			</div>
+			<div className="content">
+				<Outlet />
+			</div>
+		</div>
+	);
 }
 
-export default Layout;
+function RequireAuth() {
+	const { currentUser } = useContext(AuthContext);
+
+	if (!currentUser) {
+		return <Navigate to="/login" />;
+	}
+
+	return (
+		currentUser && (
+			<div className="layout">
+				<div className="navbar">
+					<Navbar />
+				</div>
+				<div className="content">
+					<Outlet />
+				</div>
+			</div>
+		)
+	);
+}
+
+export { Layout, RequireAuth };
