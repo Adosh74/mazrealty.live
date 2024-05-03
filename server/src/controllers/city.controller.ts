a648cf286c436dc2b3eb71a3bb6ea5645a897d0a
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-03 20:55:57 +0300

    + add update get all cities handler

--- server/src/controllers/city.controller.ts
+++ server/src/controllers/city.controller.ts
@@ -1,9 +1,19 @@
 import City from '../models/city.model';
+import catchAsync from '../utils/catchAsync.util';
 import * as Factory from './handlerFactory.controller';
 
 // *** CRUD operation for city
 
-export const getAllCity = Factory.getAll(City);
+export const getAllCity = catchAsync(async (req, res, next) => {
+	const cities = await City.find().select('-__v').sort({ city_name_en: 1 }).exec();
+	res.status(200).json({
+		status: 'success',
+		results: cities.length,
+		data: {
+			cities,
+		},
+	});
+});
 export const getOneCity = Factory.getOne(City);
 export const createOneCity = Factory.createOne(City);
 export const updateOneCity = Factory.updateOne(City);
