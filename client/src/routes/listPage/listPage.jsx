a46f2240cd819eb8c945e5f846ab51818455014e
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-08 08:30:26 +0300

    + add loader defer in list page

--- client/src/routes/listPage/listPage.jsx
+++ client/src/routes/listPage/listPage.jsx
@@ -1,25 +1,60 @@
-import { listData } from "../../lib/dummydata";
-import "./listPage.scss";
-import Filter from "../../components/filter/Filter"
-import Card from "../../components/card/Card"
-import Map from "../../components/map/Map";
+import { Suspense } from 'react';
+import { Await, useLoaderData } from 'react-router-dom';
+import Card from '../../components/card/Card';
+import Filter from '../../components/filter/Filter';
+import Map from '../../components/map/Map';
+import './listPage.scss';
 
 function ListPage() {
-  const data = listData;
-
-  return <div className="listPage">
-    <div className="listContainer">
-      <div className="wrapper">
-        <Filter/>
-        {data.map(item=>(
-          <Card key={item.id} item={item}/>
-        ))}
-      </div>
-    </div>
-    <div className="mapContainer">
-      <Map items={data}/>
-    </div>
-  </div>;
+	// get properties from loader
+	const data = useLoaderData();
+	return (
+		<div className="listPage">
+			<div className="listContainer">
+				<div className="wrapper">
+					<Filter />
+					<Suspense
+						fallback={
+							<div className="loading">
+								<h1>Loading...</h1>
+							</div>
+						}
+					>
+						<Await
+							resolve={data.properties}
+							errorElement={<p>Error loading properties</p>}
+						>
+							{(properties) => {
+								if (properties.length === 0) {
+									return <h1>No properties found</h1>;
+								} else {
+									return properties.map((property) => (
+										<Card key={property._id} item={property} />
+									));
+								}
+							}}
+						</Await>
+					</Suspense>
+				</div>
+			</div>
+			<div className="mapContainer">
+				<Suspense
+					fallback={
+						<div className="loading">
+							<h1>Loading...</h1>
+						</div>
+					}
+				>
+					<Await
+						resolve={data.properties}
+						errorElement={<p>Error loading properties</p>}
+					>
+						{(properties) => <Map items={properties} />}
+					</Await>
+				</Suspense>
+			</div>
+		</div>
+	);
 }
 
 export default ListPage;
