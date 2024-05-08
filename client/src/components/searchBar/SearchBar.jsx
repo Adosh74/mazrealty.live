62b85d4f963eb8d0986f445de6422d63f9c0ec2e
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-08 08:28:39 +0300

    + add query string to search

--- client/src/components/searchBar/SearchBar.jsx
+++ client/src/components/searchBar/SearchBar.jsx
@@ -1,55 +1,94 @@
-import { useState } from "react";
-import "./searchBar.scss";
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import apiRequest from '../../lib/apiRequest';
+import './searchBar.scss';
 
-const types = ["buy", "rent"];
+const transactions = ['sale', 'rent'];
 
 function SearchBar() {
-  const [query, setQuery] = useState({
-    type: "buy",
-    location: "",
-    minPrice: 0,
-    maxPrice: 0,
-  });
+	const [cities, setCities] = useState([]);
+	const [query, setQuery] = useState({
+		transaction: 'sale',
+		city: '',
+		minPrice: 0,
+		maxPrice: 1000000,
+	});
 
-  const switchType = (val) => {
-    setQuery((prev) => ({ ...prev, type: val }));
-  };
+	const switchType = (val) => {
+		setQuery((prev) => ({ ...prev, transaction: val }));
+	};
 
-  return (
-    <div className="searchBar">
-      <div className="type">
-        {types.map((type) => (
-          <button
-            key={type}
-            onClick={() => switchType(type)}
-            className={query.type === type ? "active" : ""}
-          >
-            {type}
-          </button>
-        ))}
-      </div>
-      <form>
-        <input type="text" name="location" placeholder="City Location" />
-        <input
-          type="number"
-          name="minPrice"
-          min={0}
-          max={10000000}
-          placeholder="Min Price"
-        />
-        <input
-          type="number"
-          name="maxPrice"
-          min={0}
-          max={10000000}
-          placeholder="Max Price"
-        />
-        <button>
-          <img src="/search.png" alt="" />
-        </button>
-      </form>
-    </div>
-  );
+	useEffect(() => {
+		async function fetchData() {
+			const res = await apiRequest.get('/cities');
+
+			setCities(res.data.data.cities);
+		}
+		fetchData();
+	}, []);
+
+	const handleChange = (e) => {
+		setQuery((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
+
+	const queryStr =
+		query.city !== ''
+			? `?transaction=${query.transaction}&city._id=${query.city}&price[gte]=${query.minPrice}&price[lte]=${query.maxPrice}`
+			: `?transaction=${query.transaction}&price[gte]=${query.minPrice}&price[lte]=${query.maxPrice}`;
+	return (
+		<div className="searchBar">
+			<div className="type">
+				{transactions.map((type) => (
+					<button
+						key={type}
+						onClick={() => switchType(type)}
+						className={query.transaction === type ? 'active' : ''}
+					>
+						{type}
+					</button>
+				))}
+			</div>
+			<form>
+				{/* <input type="text" name="location" placeholder="City Location" /> */}
+				{/* hot input from cities and choose one*/}
+				<select
+					name="city"
+					onChange={(e) =>
+						setQuery((prev) => ({ ...prev, city: e.target.value }))
+					}
+				>
+					<option value="">Select City</option>
+					{cities.map((city) => (
+						<option key={city._id} value={city._id}>
+							{city.city_name_en}
+						</option>
+					))}
+				</select>
+
+				<input
+					type="number"
+					name="minPrice"
+					min={0}
+					max={10000000}
+					placeholder="Min Price"
+					onChange={handleChange}
+				/>
+				<input
+					type="number"
+					name="maxPrice"
+					min={0}
+					max={10000000}
+					placeholder="Max Price"
+					onChange={handleChange}
+				/>
+				<Link to={`/list${queryStr}`}>
+					<button>
+						<img src="/search.png" alt="" />
+					</button>
+				</Link>
+			</form>
+		</div>
+	);
 }
 
 export default SearchBar;
