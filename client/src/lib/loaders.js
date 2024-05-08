ea2bb67179fe74b729209740968c954347c0fc5e
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-08 08:26:03 +0300

    + add list page loader

--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -1,6 +1,18 @@
+import { defer } from 'react-router-dom';
 import apiRequest from './apiRequest';
 
 export const singlePageLoader = async ({ request, params }) => {
-	const res = await apiRequest.get(`properties/${params.id}`);
+	const res = await apiRequest.get(`/properties/${params.id}`);
 	return res.data.data.data;
+};
+
+export const listPageLoader = async ({ request, params }) => {
+	const query = request.url.split('?')[1];
+
+	const res = !query
+		? apiRequest.get(`/properties`)
+		: apiRequest.get(`properties?${query}`);
+	return defer({
+		properties: res.then((res) => res.data.data.data),
+	});
 };
