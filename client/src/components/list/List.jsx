7f2897282f2809f17c92c3462924194968045813
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-10 12:12:51 +0300

    + update list component to iterate in props data

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,24 +1,10 @@
-import { useEffect, useState } from 'react';
-import apiRequest from '../../lib/apiRequest';
 import Card from '../card/Card';
 import './list.scss';
 
-function List() {
-	const [properties, setProperties] = useState([]);
-	useEffect(() => {
-		apiRequest
-			.get('/properties')
-			.then((res) => {
-				console.log(res.data.data.data);
-				setProperties(res.data.data.data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	}, []);
+function List({ items }) {
 	return (
 		<div className="list">
-			{properties.map((item) => (
+			{items.map((item) => (
 				<Card key={item._id} item={item} />
 			))}
 		</div>
