79e54e701fe279b560b99dc6eb9fd38dfcb2e862
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-10 11:31:20 +0300

    + add loader to favorite page and serve the data

--- client/src/routes/favourites/Favourites.jsx
+++ client/src/routes/favourites/Favourites.jsx
@@ -1,69 +1,60 @@
+import { Suspense } from 'react';
+import { Await, useLoaderData } from 'react-router-dom';
+import Card from '../../components/card/Card';
+import Map from '../../components/map/Map';
 import './favourites.scss';
-import { useEffect, useState } from 'react';
-import apiRequest from '../../lib/apiRequest';
-import { Link } from 'react-router-dom';
 
 function Favourites() {
-  const [properties, setProperties] = useState([]);
-	useEffect(() => {
-		apiRequest
-			.get('/properties')
-			.then((res) => {
-				console.log(res.data.data.data);
-				setProperties(res.data.data.data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	}, []);
-		return (
+	const data = useLoaderData();
+	return (
 		<div className="favouritePage">
 			<div className="details">
 				<div className="wrapper">
 					<div className="title">
 						<h1>Saved List</h1>
 					</div>
-					<div className="list">
-			{properties.map((item) => (
-			<div className="card">
-			<Link to={`/${item._id}`} className="imageContainer">
-				<img src={item.images[0]} alt="" />
-			</Link>
-			<div className="textContainer">
-				<h2 className="title">
-					<Link to={`/${item._id}`}>{item.name}</Link>
-				</h2>
-				<p className="address">
-					<img src="/pin.png" alt="" />
-					<span>{item.city.city_name_en}</span>
-				</p>
-				<p className="price">$ {item.price}</p>
-				<div className="bottom">
-					<div className="features">
-						<div className="feature">
-							<img src="/bed.png" alt="" />
-							<span>{item.bedrooms} bedroom</span>
-						</div>
-						<div className="feature">
-							<img src="/bath.png" alt="" />
-							<span>{item.bathrooms} bathroom</span>
-						</div>
-					</div>
-					<div className="icons">
-						
-						<div className="icon">
-							<img src="/chat.png" alt="" />
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-			))}
-		</div>
+					<Suspense fallback={<div>Loading...</div>}>
+						<Await
+							resolve={data.favorites}
+							errorElement={<div>Failed to load properties</div>}
+						>
+							{(favorites) => {
+								console.log(favorites);
+								if (favorites.length === 0) {
+									return <h1>No properties found</h1>;
+								} else {
+									return favorites.map((favorite) => (
+										<Card
+											key={favorite._id}
+											item={favorite.property}
+										/>
+									));
+								}
+							}}
+						</Await>
+					</Suspense>
 				</div>
 			</div>
 			<div className="rightPage">
+				<Suspense
+					fallback={
+						<div className="loading">
+							<h1>Loading...</h1>
+						</div>
+					}
+				>
+					<Await
+						resolve={data.favorites}
+						errorElement={<p>Failed to load properties</p>}
+					>
+						{(favorites) => {
+							const properties = favorites.map(
+								(favorite) => favorite.property
+							);
+							return <Map items={properties} />;
+						}}
+					</Await>
+				</Suspense>
 			</div>
 		</div>
 	);
