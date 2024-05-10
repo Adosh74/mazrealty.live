57ea9d594d7739a5b3e46472d25e119a9634e412
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-10 10:06:44 +0300

    + add my favorite property loader to favorites + update single property route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,19 @@
+import { Toaster } from 'react-hot-toast';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { singlePageLoader, listPageLoader } from './lib/loaders';
+import { singlePageLoader, listPageLoader, myPropertiesLoader } from './lib/loaders';
 import EditProperty from './routes/editMyProperty/editMyProperty';
+import Favourites from './routes/favourites/Favourites';
 import HomePage from './routes/homePage/homePage';
+import LawyerPage from './routes/lawyerPage/LawyerPage';
 import { Layout, RequireAuth } from './routes/layout/layout';
 import ListPage from './routes/listPage/listPage';
 import Login from './routes/login/login';
 import NewPostPage from './routes/newPostPage/newPostPage';
+import PageNotFound from './routes/pageNotFound/PageNotFound';
 import ProfilePage from './routes/profilePage/profilePage';
 import ProfileUpdatePage from './routes/profileUpdatePage/profileUpdatePage';
 import Register from './routes/register/register';
 import SinglePage from './routes/singlePage/singlePage';
-import LawyerPage from './routes/lawyerPage/LawyerPage';
-import PageNotFound from './routes/pageNotFound/PageNotFound';
-import Favourites from './routes/favourites/Favourites';
-import {Toaster} from "react-hot-toast";
 
 function App() {
 	const router = createBrowserRouter([
@@ -31,7 +31,7 @@ function App() {
 					loader: listPageLoader,
 				},
 				{
-					path: '/:id',
+					path: 'property/:id',
 					element: <SinglePage />,
 					loader: singlePageLoader,
 				},
@@ -69,22 +69,25 @@ function App() {
 					path: '/lawyer',
 					element: <LawyerPage />,
 				},
-				,
 				{
 					path: '/favourites',
 					element: <Favourites />,
+					loader: myPropertiesLoader,
 				},
 			],
 		},
 		{
 			path: '*',
 			element: <PageNotFound />,
-			
 		},
 	]);
 
-	return (<><RouterProvider router={router} /><Toaster />
-</>);
+	return (
+		<>
+			<RouterProvider router={router} />
+			<Toaster />
+		</>
+	);
 }
 
 export default App;
