a230a9d45b95d1e6f8e248b2614d95b95aec955e
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-13 09:15:53 +0300

    + handle approved property client side + handle reject property client side

--- client/src/components/cardForLawyer/CardForLawyer.jsx
+++ client/src/components/cardForLawyer/CardForLawyer.jsx
@@ -1,71 +1,157 @@
-import { Link } from "react-router-dom";
-import "./cardForLawyer.scss";
-import {Trash2,Eye,BookCheck} from "lucide-react";
-import toast from "react-hot-toast";
-
+import { Trash2, Eye, BookCheck } from 'lucide-react';
+import { useEffect } from 'react';
+import toast from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
+import apiRequest from '../../lib/apiRequest';
+import './cardForLawyer.scss';
 
 function CardForLawyer({ item }) {
+	const navigate = useNavigate();
 
-  
-      const handelReject = async () => {
-        try{
-        toast.success('It has been rejected',{ style: {
-      borderRadius: '10px',
-      background: '#333',
-      color: '#fff',
-    },})
-  }
-    catch (error) {
-            toast.error('Failed to rejecte property');
-        }
-      }
+	// render the component after the property has been rejected
+	useEffect(() => {
+		if (item.isRejected) {
+			toast.success('It has been rejected', {
+				style: {
+					borderRadius: '10px',
+					background: '#333',
+					color: '#fff',
+				},
+			});
+		}
+	}, [item.isRejected]);
 
-      const handelAccept = async () => {
-        toast.success('It has been accepted')
-      }
+	const handelReject = async (propertyId) => {
+		try {
+			await apiRequest.delete(`lawyers/reject-property/${propertyId}`);
+			toast.success('The property has been rejected ✔', {
+				style: {
+					border: '1px solid #713200',
+					padding: '16px',
+					paddingLeft: '25px',
+					paddingRight: '25px',
+					color: '#3ddb55',
+				},
+				iconTheme: {
+					primary: '#3ddb55',
+					secondary: '#FFFAEE',
+				},
+			});
 
-  return (
-    <div className="cardLawyer">
-      <Link to={`/property/${item._id}`} className="imageContainer">
-        <img src={item.img} alt="" />
-      </Link>
-      <div className="cardLeft">
-        <h2 className="title">
-          <Link to={`/property/${item._id}`}>{item.title}</Link>
-        </h2>
-        <p className="address">
-          <img src="/pin.png" alt="" />
-          <span>{item.address}</span>
-        </p>
-        <p className="price">$ {item.price}</p>
-        <div className="bottom">
-          <div className="features">
-            <div className="feature">
-              <img src="/bed.png" alt="" />
-              <span>{item.bedroom} bedroom</span>
-            </div>
-            <div className="feature">
-              <img src="/bath.png" alt="" />
-              <span>{item.bathroom} bathroom</span>
-            </div>
-          </div>
-          <div className="icons">
-            
-            <div className="icon">
-              <img src="/chat.png" alt="" />
-            </div>
-          </div>
-        </div>
-      </div>
-    
-      <div className="cardRight">
-        <button onClick={handelReject} className="rejectButton"><div></div><Trash2 /></button> 
-        <button onClick={handelAccept} className="acceptButton"><BookCheck /></button>
-        <Link to={item.img}><button className="viewButton"><Eye /><span>view contract</span></button></Link>
-      </div>
-  </div>
-    
-  );
+			// rerender the component after the property has been rejected
+			navigate('/lawyer', { replace: true });
+		} catch (error) {
+			console.log(error);
+			toast.error('Failed to reject property', {
+				style: {
+					border: '1px solid #713200',
+					padding: '16px',
+					paddingLeft: '25px',
+					paddingRight: '25px',
+					color: '#713200',
+				},
+				iconTheme: {
+					primary: '#713200',
+					secondary: '#FFFAEE',
+				},
+			});
+		}
+	};
+
+	const handelAccept = async (propertyId) => {
+		try {
+			await apiRequest.patch(`/lawyers/approve-property/${propertyId}`);
+			toast.success('The property has been accepted ✔', {
+				style: {
+					border: '1px solid #713200',
+					padding: '16px',
+					paddingLeft: '25px',
+					paddingRight: '25px',
+					color: '#3ddb55',
+				},
+				iconTheme: {
+					primary: '#3ddb55',
+					secondary: '#FFFAEE',
+				},
+			});
+
+			navigate(`/property/${propertyId}`);
+		} catch (error) {
+			console.log(error);
+			toast.error('Failed to accept property', {
+				style: {
+					border: '1px solid #713200',
+					padding: '16px',
+					paddingLeft: '25px',
+					paddingRight: '25px',
+					color: '#713200',
+				},
+				iconTheme: {
+					primary: '#713200',
+					secondary: '#FFFAEE',
+				},
+			});
+		}
+	};
+
+	const handleViewContract = (contractUrl) => {
+		// popup window to view contract
+		window.open(contractUrl, '_blank');
+	};
+
+	return (
+		<div className="cardLawyer">
+			<Link to={`/property/${item._id}`} className="imageContainer">
+				<img src={item.images[0]} alt="" />
+			</Link>
+			<div className="cardLeft">
+				<h2 className="title">
+					<Link to={`/property/${item._id}`}>{item.name}</Link>
+				</h2>
+				<p className="address">
+					<img src="/pin.png" alt="" />
+					<span>{item.address}</span>
+				</p>
+				<p className="price">$ {item.price}</p>
+				<div className="bottom">
+					<div className="features">
+						<div className="feature">
+							<img src="/bed.png" alt="" />
+							<span>{item.bedrooms} bedroom</span>
+						</div>
+						<div className="feature">
+							<img src="/bath.png" alt="" />
+							<span>{item.bathrooms} bathroom</span>
+						</div>
+					</div>
+					<div className="icons">
+						<div className="icon">
+							<img src="/chat.png" alt="" />
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="cardRight">
+				<button onClick={() => handelReject(item._id)} className="rejectButton">
+					<div></div>
+					<Trash2 />
+				</button>
+				<button onClick={() => handelAccept(item._id)} className="acceptButton">
+					<BookCheck />
+				</button>
+				<Link to={item.img}>
+					<button
+						className="viewButton"
+						onClick={() => handleViewContract(item.contract)}
+					>
+						<Eye />
+						<span>view contract</span>
+					</button>
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default CardForLawyer;
