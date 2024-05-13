d093c9c91f6f91cc2c4eff7e000f70961d4fcc1b
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-13 09:14:46 +0300

    + add reject property functionality + add sending email where property approved and reject

--- server/src/controllers/lawyer.controller.ts
+++ server/src/controllers/lawyer.controller.ts
@@ -1,8 +1,11 @@
+import ejs from 'ejs';
 import { NextFunction, Request, Response } from 'express';
+import path from 'path';
 import ApprovedProp from '../models/approvedProp.model';
 import Property from '../models/property.model';
 import AppError from '../utils/AppError.util';
 import catchAsync from '../utils/catchAsync.util';
+import sendMail from '../utils/mail';
 
 export const getNotApproved = catchAsync(
 	async (req: Request, res: Response, next: NextFunction) => {
@@ -49,9 +52,64 @@ export const approveProperty = catchAsync(
 			property: propertyId,
 		});
 
+		// const data = {
+		// 	user: { name: property.owner.name },
+		// 	property: {
+		// 		name: property.name,
+		// 		type: property.type,
+		// 		address: property.address,
+		// 		price: property.price,
+		// 	},
+		// };
+
+		// await ejs.renderFile(path.join(process.cwd(), 'src/mails/approved.ejs'), data);
+
+		// await sendMail({
+		// 	email: property.owner.email as string,
+		// 	subject: 'Property Approved 🚀✔',
+		// 	template: 'approved.ejs',
+		// 	data,
+		// });
+
 		res.status(200).json({
 			status: 'success',
 			data: { property },
+		});
+	}
+);
+
+export const rejectProperty = catchAsync(
+	async (req: Request, res: Response, next: NextFunction) => {
+		const { propertyId } = req.params;
+
+		const property = await Property.findByIdAndDelete(propertyId);
+
+		if (!property) {
+			return next(new AppError('Property not found', 404));
+		}
+
+		// const data = {
+		// 	user: { name: property.owner.name },
+		// 	property: {
+		// 		name: property.name,
+		// 		type: property.type,
+		// 		address: property.address,
+		// 		price: property.price,
+		// 	},
+		// };
+
+		// await ejs.renderFile(path.join(process.cwd(), 'src/mails/reject.ejs'), data);
+
+		// await sendMail({
+		// 	email: property.owner.email as string,
+		// 	subject: 'Property Rejected 🚫❌',
+		// 	template: 'reject.ejs',
+		// 	data,
+		// });
+
+		res.status(204).json({
+			status: 'success',
+			data: null,
 		});
 	}
 );
