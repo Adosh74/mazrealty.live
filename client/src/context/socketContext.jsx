5c3a649140b631c854097705f10f77ee570b8eaa
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-13 07:01:20 +0300

    + add socket context

---
+++ client/src/context/socketContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import { AuthContext } from './authContext';
+
+export const SocketContext = createContext();
+
+export const SocketContextProvider = ({ children }) => {
+	const { currentUser } = useContext(AuthContext);
+
+	const [socket, setsocket] = useState(null);
+
+	useEffect(() => {
+		setsocket(io('http://localhost:4000/'));
+	}, []);
+
+	useEffect(() => {
+		currentUser && socket?.emit('newUser', currentUser._id);
+	}, [currentUser, socket]);
+	return <SocketContext.Provider value={{ socket }}>{children}</SocketContext.Provider>;
+};
