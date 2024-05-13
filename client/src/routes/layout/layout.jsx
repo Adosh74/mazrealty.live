399c174b811a6b6ef86146efa3f4c1a3f05f6206
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-13 09:23:16 +0300

    + add RequireAdminOrLayer layout

--- client/src/routes/layout/layout.jsx
+++ client/src/routes/layout/layout.jsx
@@ -38,4 +38,28 @@ function RequireAuth() {
 	);
 }
 
-export { Layout, RequireAuth };
+function RequireAdminOrLayer() {
+	const { currentUser } = useContext(AuthContext);
+	if (!currentUser) {
+		return <Navigate to="/login" />;
+	}
+
+	if (currentUser.role !== 'admin' && currentUser.role !== 'lawyer') {
+		return <Navigate to="/" />;
+	}
+
+	return (
+		currentUser && (
+			<div className="layout">
+				<div className="navbar">
+					<Navbar />
+				</div>
+				<div className="content">
+					<Outlet />
+				</div>
+			</div>
+		)
+	);
+}
+
+export { Layout, RequireAuth, RequireAdminOrLayer };
