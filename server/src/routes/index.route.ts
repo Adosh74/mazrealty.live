0cc02bc96d4a9b773f310ece267cb869c882fb56
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-13 06:59:02 +0300

    + add messages and chats endpoints to main routes

--- server/src/routes/index.route.ts
+++ server/src/routes/index.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import adminRoutes from './APIs/admin.route';
 import authRoutes from './APIs/auth.route';
+import chatRoutes from './APIs/chat.route';
 import cityRoutes from './APIs/city.route';
 import lawyerRoutes from './APIs/lawyer.route';
+import messageRoutes from './APIs/message.route';
 import propertyRoutes from './APIs/property.route';
 import userRoutes from './APIs/user.route';
 import userFavoriteRoutes from './APIs/userFavorite.route';
@@ -18,5 +20,7 @@ routes.use('/favorites', userFavoriteRoutes);
 routes.use('/cities', cityRoutes);
 routes.use('/lawyers', lawyerRoutes);
 routes.use('/admin', adminRoutes);
+routes.use('/chats', chatRoutes);
+routes.use('/messages', messageRoutes);
 
 export default routes;
