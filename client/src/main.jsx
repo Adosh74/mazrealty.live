f735ccacd06af323f463465de62e0e3fc84751f8
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-13 07:01:57 +0300

    + add socket context provider to main app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { AuthContextProvider } from './context/authContext.jsx';
+import { SocketContextProvider } from './context/socketContext.jsx';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<AuthContextProvider>
-			<App />
+			<SocketContextProvider>
+				<App />
+			</SocketContextProvider>
 		</AuthContextProvider>
 	</React.StrictMode>
 );
