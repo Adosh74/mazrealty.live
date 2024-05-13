d1285574d8dec1a15fb985c2b178f4b583aee152
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-13 06:56:09 +0300

    + add message routes

---
+++ server/src/routes/APIs/message.route.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { protect } from '../../controllers/auth.controller';
+import { addMessage } from '../../controllers/message.controller';
+
+const router = Router();
+
+// protect all message routes
+router.use(protect);
+
+router.post('/:chatId', addMessage);
+
+export default router;
