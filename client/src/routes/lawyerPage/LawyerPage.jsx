af25ab5477ec8a8369ced27d15484f525b3f09b5
Mohamed Shebl <hamdysheblm@gmail.com>
2024-05-13 09:16:26 +0300

    + integrate lawyer route with api

--- client/src/routes/lawyerPage/LawyerPage.jsx
+++ client/src/routes/lawyerPage/LawyerPage.jsx
@@ -1,19 +1,47 @@
-import "./LawyerPage.scss";
-import CardForLawyer from"../../components/cardForLawyer/CardForLawyer"
-import {listData} from"../../lib/dummydata"
-
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import CardForLawyer from '../../components/cardForLawyer/CardForLawyer';
+import apiRequest from '../../lib/apiRequest';
+import { listData } from '../../lib/dummydata';
+import './LawyerPage.scss';
 
 function LawyerPage() {
+	const [properties, setProperties] = useState([]);
 
-  return  <div className="profilePage">
-            <div className='list'>
-            {listData.map(item=>( 
-                <CardForLawyer key={item.id} item={item}/>  
-            ))}
-            </div>	
-	      </div>
-            
+	useEffect(() => {
+		const getProperties = async () => {
+			try {
+				const res = await apiRequest.get('/lawyers/not-approved');
+				setProperties(res.data.data.propertyNotApproved);
+			} catch (error) {
+				console.log(error);
+				toast.error(`${error.response.data.message}`, {
+					style: {
+						border: '1px solid #713200',
+						padding: '16px',
+						paddingLeft: '25px',
+						paddingRight: '25px',
+						color: '#713200',
+					},
+					iconTheme: {
+						primary: '#713200',
+						secondary: '#FFFAEE',
+					},
+				});
+			}
+		};
+		getProperties();
+	}, []);
+
+	return (
+		<div className="profilePage">
+			<div className="list">
+				{properties.map((item) => (
+					<CardForLawyer key={item._id} item={item} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default LawyerPage;
-
